0ae691f15303e896b0bf9f58cf0022e2c3f6b50a
Jakub Szot <kuszot123@gmail.com>
2023-12-11 23:56:14 +0100

    added icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,16 @@
+import twittericon from './twitter.svg';
+import instagram from './instagram.svg';
+import facebook from './facebook.svg';
+import rightArrow from './rightArrow.png';
+import spoonAndForkIcon from './spoonFork.svg';
+import clockIcon from './clockIcon.svg';
+
+
+export{
+    twittericon,
+    instagram,
+    facebook,
+    rightArrow,
+    spoonAndForkIcon,
+    clockIcon
+}
