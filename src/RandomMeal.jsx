30c14b2488f4ba85b28e7da68b4d299e8669fe4a
Jakub Szot <kuszot123@gmail.com>
2023-12-16 18:09:50 +0100

    updated name for destructuring title

--- src/RandomMeal.jsx
+++ src/RandomMeal.jsx
@@ -8,8 +8,8 @@ const RandomMeal = () => {
     useEffect(() => {
         const fetchData = async () => {
         try {
-            const { title, image, url } = await generateRandomMeal();
-            setMealData({ title, image, url });
+            const { label, image, url } = await generateRandomMeal();
+            setMealData({ label, image, url });
         } catch (error) {
             console.error('Error fetching random meal', error);
         }
@@ -23,13 +23,13 @@ const RandomMeal = () => {
         return <p>Loading...</p>;
     }
 
-    const { title, image} = mealData;
+    const { label, image} = mealData;
     return (
         <section className='flex my-16 max-container max-h-[40rem]'>
             <div className='flex flex-1'>
                 <div className='bg-gray flex-1 overflow-hidden'>
                     <div className='pt-32 pl-20 flex flex-col pr-5 justify-around h-full'>
-                        <h2 className='font-oswald font-medium text-6xl break-words max-w-sm'>{title}</h2>
+                        <h2 className='font-oswald font-medium text-6xl break-words max-w-sm'>{label}</h2>
                         <p className='font-roboto text-justify break-words'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem non corrupti aspernatur nemo exercitationem! Atque explicabo delectus commodi nihil, tempore cupiditate nobis perferendis consectetur sapiente, molestias quae consequatur doloremque exercitationem!</p>
                         <div className='flex justify-start'>
                             <button className='bg-black text-white py-4 px-8 rounded-2xl flex justify-center items-center gap-4'>View Recipe <img src={rightArrow} alt="right arrow" height={25} width={25} /></button>
