b770a98e36d5785b7afc7088574f51a4bc495e8d
Jakub Szot <kuszot123@gmail.com>
2023-12-27 01:46:43 +0100

    Refactor MealCard component to accept mealData prop

--- src/MealCard.jsx
+++ src/MealCard.jsx
@@ -1,44 +1,14 @@
-import generateRandomMeal from './generateRandomMeal'
-import { useEffect, useState } from 'react';
 import { clockIcon,spoonAndForkIcon } from './assets/icons';
-
-const MealCard = () => {
-    const [mealImg, setMealImg] = useState(null);
-    const [mealType, setMealType] = useState(null);
-    const [mealTime, setMealTime] = useState(null);
-
-    useEffect((id) => {
-        const fetchData = async () => {
-        try {
-            const {image, mealType, totalTime} = await generateRandomMeal(id);
-            setMealImg(image);
-            setMealType(mealType);
-            setMealTime(totalTime);
-        } catch (error) {
-            console.error('Error fetching random meal', error);
-        }
-        };
-
-        fetchData();
-    }, []); // Empty dependency array ensures this effect runs once on component mount
-    
-    const image = mealImg;
-    const type = mealType;
-    const time = mealTime;
-    
-
-    if (!image || !type) {
-        // Optional: Add a loading state or handle the absence of data
-        return <p>Loading...</p>;
-    }
+const MealCard = ({mealData}) => {
+    const {img,totalTime,mealType} = mealData;
 
     return (
         <div className='font-roboto'>
             <div className='flex flex-col w-[350px]'>
-                <img src={image} alt="image of meal" className='w-full'/>
+                <img src={img} alt="image of meal" className='w-full'/>
                 <div className='bg-gray flex justify-evenly items-center p-4 font-medium'>
-                    <div className='flex items-center gap-1'><img src={clockIcon} alt="clock icon" height={25} width={25}/>{time >0 ? time: "???"} minutes</div>
-                    <div className='flex items-center gap-1'><img src={spoonAndForkIcon} alt="icon of spoon and fork" height={25} width={25}/>{type}</div>
+                    <div className='flex items-center gap-1'><img src={clockIcon} alt="clock icon" height={25} width={25}/>{totalTime >0 ? totalTime: "???"} minutes</div>
+                    <div className='flex items-center gap-1'><img src={spoonAndForkIcon} alt="icon of spoon and fork" height={25} width={25}/>{mealType}</div>
                 </div>
             </div>
         </div>
