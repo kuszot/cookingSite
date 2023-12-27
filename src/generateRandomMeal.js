5cea685b80515af6bb62d21af482ae3c84e2d1c9
Jakub Szot <kuszot123@gmail.com>
2023-12-27 01:55:34 +0100

    changed logic of function to return an array of recipes

--- src/generateRandomMeal.js
+++ src/generateRandomMeal.js
@@ -1,11 +1,16 @@
 import fetchRandomMeal from './fetchRandomMeal';
 
 
-async function generateRandomMeal(id=0) {
+async function generateRandomMeal() {
     try{
       const response = await fetchRandomMeal();
-      const { recipe } = response[id];
-      return recipe;
+
+      const recipes = [];
+      // create array of recipes that contains object of recipe
+      for (let i = 0; i < response.length; i++) {
+        recipes.push({label:response[i].recipe.label, img:response[i].recipe.image, url:response[i].recipe.url, totalTime:response[i].recipe.totalTime, mealType:response[i].recipe.mealType})
+      }
+      return recipes;
     } catch (error) {
         console.error('Error generating random meal' + error);
         return null;
